b1cbf533a8cd72f2c23fce2f287fab9dc7ee62f6
Dorothy Rifai <drifai28@gmail.com>
2020-07-23 20:52:10 -0400

    Initializing the state machine with the payment status from the db.

--- src/main/java/guru/springframework/services/PaymentServiceImpl.java
+++ src/main/java/guru/springframework/services/PaymentServiceImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.services;
 
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.domain.Payment;
@@ -24,20 +25,35 @@ public class PaymentServiceImpl implements PaymentService {
 
 	@Override
 	public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
-		// TODO Auto-generated method stub
+		StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
 		return null;
 	}
 
 	@Override
 	public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
-		// TODO Auto-generated method stub
+		StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
 		return null;
 	}
 
 	@Override
 	public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
-		// TODO Auto-generated method stub
+		StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
 		return null;
+	}
+	
+	private StateMachine<PaymentState, PaymentEvent> build(Long paymentId) {
+		Payment payment = paymentRepository.getOne(paymentId);
+		
+		StateMachine<PaymentState, PaymentEvent> sm = stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+		sm.stop();
+		// sets it to status from database
+		sm.getStateMachineAccessor()
+			.doWithAllRegions(sma -> {
+				sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null));
+			});
+		
+		sm.start();
+		return sm;
 	}
 
 }
